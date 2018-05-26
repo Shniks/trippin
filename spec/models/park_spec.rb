9ad7d422c522f4f5818e4c12357754424bb27a79
Nikhil Shahi <shniks@gmail.com>
2018-05-26 17:53:18 -0600

    Added validations in the model test for the park model.

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Park, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{ is_expected.to validate_presence_of(:latitude) }
+  it{ is_expected.to validate_presence_of(:longitude) }
+  it{ is_expected.to validate_presence_of(:full_name) }
 end
