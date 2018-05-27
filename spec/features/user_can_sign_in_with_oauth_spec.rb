d9afd7f02277171fab7dc4b6f42d5bf9ef5721ea
Nikhil Shahi <shniks@gmail.com>
2018-05-26 20:30:27 -0600

    Created feature test for logging in with Google.

---
+++ spec/features/user_can_sign_in_with_oauth_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'As a User' do
+  describe 'When I click on \'Sign in with Google\' on the homepage ' do
+    scenario 'I get logged into the app and see my name and a logout link' do
+      visit '/'
+
+      expect(page.status_code).to eq(200)
+
+      click_link "Sign in with Google"
+
+      expect(current_path).to eq(search_path)
+      expect(page).to have_css('.welcome_profile')
+      expect(page).to have_content('Nikhil')
+      expect(page).to have_link('Logout')
+    end
+  end
+end
