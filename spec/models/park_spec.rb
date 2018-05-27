81ae5590f9b477d4b9c711aea73d5fe137e4fd10
Nikhil Shahi <shniks@gmail.com>
2018-05-26 18:02:15 -0600

    Added factory for the park model.

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Park, type: :model do
-  it{ is_expected.to validate_presence_of(:latitude) }
-  it{ is_expected.to validate_presence_of(:longitude) }
-  it{ is_expected.to validate_presence_of(:full_name) }
+  describe 'Validations' do
+    it{ is_expected.to validate_presence_of(:latitude) }
+    it{ is_expected.to validate_presence_of(:longitude) }
+    it{ is_expected.to validate_presence_of(:full_name) }
+  end
+
+  describe 'Class Methods' do
+    describe '.create_parks' do
+      it 'Creates a park object' do
+        park = create!(:park)
+
+        binding.pry
+      end
+    end
+  end 
+
 end
