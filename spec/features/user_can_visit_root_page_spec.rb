7d4479058661a31e48bd230e013a7bfadf57e57a
Nikhil Shahi <shniks@gmail.com>
2018-05-26 19:25:10 -0600

    Changed the feature test along with the application.html.erb file and the application.css file to only log in with google.

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 describe 'As a User' do
   describe 'When I visit the root page' do
-    scenario 'I can view the navbar contents with the register and login links' do
+    scenario 'I can view the navbar contents with the sign in links' do
       visit root_path
 
       within('.navbar') do
-        expect(page).to have_link('New User')
-        expect(page).to have_link('Login')
+        expect(page).to have_link('Sign in with Google')
       end
     end
   end
