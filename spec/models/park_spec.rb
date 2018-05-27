725da5f0b39be4684a2a974b1d1d7d461753d0e6
Nikhil Shahi <shniks@gmail.com>
2018-05-26 18:15:52 -0600

    Created passing class test for the park model.

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -10,11 +10,23 @@ RSpec.describe Park, type: :model do
   describe 'Class Methods' do
     describe '.create_parks' do
       it 'Creates a park object' do
-        park = create!(:park)
+        info = { states: 'Colorado',
+                 description: 'Rocky Mountain National Park',
+                 directionsInfo: 'Go to Boulder and make a left to Estes Park',
+                 directionsUrl: 'http://awesomerockymountains.com/directions',
+                 fullName: 'Rocky Mountain National Park',
+                 url: 'http://awesomerockymountains.com',
+                 weather_info: 'Partly cloudy with a chance of shower',
+                 latLong: 'lat:40.3428, long:-105.6836' }
 
-        binding.pry
+        park = Park.create_parks(info)
+        
+        expect(park.state).to eq('Colorado')
+        expect(park.latitude).to eq('40.3428')
+        expect(park.longitude).to eq('-105.6836')
+        expect(park.full_name).to eq('Rocky Mountain National Park')
       end
     end
-  end 
+  end
 
 end
