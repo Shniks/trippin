b41e0f1f8ceb316985af35b312cd69772db4edee
Nikhil Shahi <shniks@gmail.com>
2018-05-28 02:59:01 -0600

    Added routes for google oauth, callback and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/search', to: 'search#index'
+
+  get '/auth/google_oauth2', as: :google_login
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
