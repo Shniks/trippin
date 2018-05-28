5c7fcbef224083927a6db4df9ce44b5c53d046ed
Nikhil Shahi <shniks@gmail.com>
2018-05-28 06:24:51 -0600

    Added test for user model.

--- spec/features/user_can_sign_in_with_oauth_spec.rb
+++ spec/features/user_can_sign_in_with_oauth_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 describe 'As a User' do
   describe 'When I click on \'Sign in with Google\' on the homepage ' do
     scenario 'I get logged into the app and see my name and a logout link' do
-      visit '/'
+      stub_omniauth
+      visit root_path
 
       expect(page.status_code).to eq(200)
+      expect(page).to have_link('Sign in with Google')
 
       click_link 'Sign in with Google'
 
       expect(current_path).to eq(search_path)
-      expect(page).to have_css('.welcome_profile')
       expect(page).to have_content('Nikhil')
       expect(page).to have_link('Logout')
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,36 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
    it { should validate_presence_of(:name) }
    it { should validate_presence_of(:uid) }
-   it { should validate_presence_of(:screen_name) }
+   it { should validate_presence_of(:email) }
    it { should validate_presence_of(:oauth_token) }
+ end
+
+ describe 'Class Methods' do
+   describe '.from_omniauth' do
+     it 'creates or updates itself from an oauth hash' do
+       auth_info = { "provider"=>"google_oauth2",
+                     "uid"=>"103746745442750564186",
+                     "info"=>
+                      {"name"=>"Nikhil Shahi",
+                       "email"=>"nikhil@nikhil.com",
+                       "first_name"=>"Nikhil",
+                       "last_name"=>"Shahi",
+                       "image"=>
+                        "https://lh5.googleusercontent.com/-QnXkBbmIURs/AAAAAAAAAAI/AAAAAAAABG8/HCK95bYYG_c/photo.jpg"},
+                     "credentials"=>
+                      {"token"=>
+                        "QQya29.GlvJBeUXUKW26c8C7JyykvxYm5pTiJR-EEYh9Ej65vI-AVDSEtezwCr6dSHLPmRqRIu3DsDF-OD1fjzPXgZ7P4ZeNR_5AQHg9Js",
+                       "expires_at"=>1527511552,
+                       "expires"=>true}
+                    }
+        User.from_omniauth(auth_info)
+        user = User.first
+
+        expect(user.uid).to eq('103746745442750564186')
+        expect(user.name).to eq('Nikhil Shahi')
+        expect(user.email).to eq('nikhil@nikhil.com')
+        expect(user.oauth_token).to eq('QQya29.GlvJBeUXUKW26c8C7JyykvxYm5pTiJR-EEYh9Ej65vI-AVDSEtezwCr6dSHLPmRqRIu3DsDF-OD1fjzPXgZ7P4ZeNR_5AQHg9Js')
+      end
+   end
  end
 end
