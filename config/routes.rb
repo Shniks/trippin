e9e34a9d7a7b41dff750b28dd50b210c0785a8ca
Nikhil Shahi <shniks@gmail.com>
2018-05-28 21:37:06 -0600

    Added parks path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/search', to: 'search#index'
+  get '/parks', to: 'parks#index'
 
   get '/auth/google_oauth2', as: :google_login
   get '/auth/google_oauth2/callback', to: 'sessions#create'
