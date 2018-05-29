2da87692cd9b6f4ac415d09dfb7fc8b89dcb6c68
Nikhil Shahi <shniks@gmail.com>
2018-05-28 21:36:45 -0600

    Added feature test to get a list of parks upon entering an address.

--- spec/features/user_can_see_location_and_parks_within_a_radius_spec.rb
+++ spec/features/user_can_see_location_and_parks_within_a_radius_spec.rb
@@ -4,10 +4,24 @@ describe 'As a User' do
   describe 'I log into the app' do
     describe 'And I enter my street, city, state address into the search fields' do
       describe 'And I select a radius in miles from a dropdown' do
-        scenario 'I see my location on a map along with the national parks that fall within the specified radius' do
-          visit root_path
+        describe 'I get redirected to the parks index' do
+          scenario 'I see my location on a map along with the national parks that fall within the specified radius' do
+            VCR.use_cassette('park_search_data') do
+              visit root_path
 
-          click_link 'Sign in with Google'
+              click_link 'Sign in with Google'
 
-          fill_in :street
+              fill_in :street, with: '1777 Wewatta Street'
+              fill_in :city, with: 'Denver'
+              fill_in :state, with: 'Colorado'
+
+              click_on 'Submit'
+
+              expect(current_path).to eq(parks_path)
+            end
+          end
+        end
+      end
     end
+  end
+end
