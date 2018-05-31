5687d06d469aee3ba94c8ec8c07fcae48c59c599
Nikhil Shahi <shniks@gmail.com>
2018-05-31 12:02:09 -0600

    Updated to fetch parks data for the search parameters.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,7 @@
 class SearchController < ApplicationController
   def index
     unless params[:street].nil?
-    # binding.pry
-      address = "#{params[:street]}, #{params[:city]}, #{params[:state]}"
-      lat = Geocoder.search(address).first.data["geometry"]["location"]["lat"]
-      long = Geocoder.search(address).first.data["geometry"]["location"]["lng"]
-      binding.pry
-      @parks = Park.near([lat, long], params[:radius])
+      @parks = Park.parks_search(params)
     end
   end
 end
