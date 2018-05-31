e07a61e389330beeb36bd68304ce38414439ce3b
Nikhil Shahi <shniks@gmail.com>
2018-05-31 12:01:32 -0600

    Added geocoder method to the parks model.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
+    unless params[:street].nil?
     # binding.pry
-    check = "#{params[:street]}, #{params[:city]}, #{params[:state]}"
-    # binding.pry
+      address = "#{params[:street]}, #{params[:city]}, #{params[:state]}"
+      lat = Geocoder.search(address).first.data["geometry"]["location"]["lat"]
+      long = Geocoder.search(address).first.data["geometry"]["location"]["lng"]
+      binding.pry
+      @parks = Park.near([lat, long], params[:radius])
+    end
   end
 end
