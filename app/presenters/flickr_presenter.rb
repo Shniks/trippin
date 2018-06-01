e6341f57da20954e8aa31dce39f7a375ba0eac52
Nikhil Shahi <shniks@gmail.com>
2018-06-01 05:10:43 -0600

    Created a presented to fetch the flickr photos.

--- app/presenters/flickr_presenter.rb
+++ app/presenters/flickr_presenter.rb
@@ -5,11 +5,17 @@ class FlickrPresenter
   end
 
   def photos
-    raw_photos.map { |raw_photo| Photo.new(raw_photo) }
+    create_photo_objects
   end
 
   private
     def raw_photos
       raw_photos ||= FlickrService.new(@lat, @long).photos_search
+    end
+
+    def create_photo_objects
+      raw_photos.map do |raw_photo|
+        Photo.new(raw_photo) if raw_photo.keys.include?(:url_l)
+      end.compact
     end
 end
