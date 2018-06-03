4f2b12285960ab6b838ed9c0886b80687d988cbb
Nikhil Shahi <shniks@gmail.com>
2018-06-03 14:41:10 -0600

    Adding js to try and get the map and list views to toggle.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require_tree .
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require rails-ujs
