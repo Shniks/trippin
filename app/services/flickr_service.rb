0abfae0298a2fd2de54f7b3132c523d29b43a99e
Nikhil Shahi <shniks@gmail.com>
2018-06-05 16:44:16 -0600

    Changing limit on the flickr api for photos.

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -6,7 +6,7 @@ class FlickrService
   end
 
   def photos_search
-    get_json("rest/?method=flickr.photos.search&api_key=#{ENV['FLICKR_KEY']}&accuracy=8&per_page=500&has_geo=1&lat=#{@lat}&lon=#{@long}&radius=20&is_getty=1&extras=geo%2Cowner_name%2C+url_s%2C+url_sq%2C&views%2C&format=json&nojsoncallback=1")
+    get_json("rest/?method=flickr.photos.search&api_key=#{ENV['FLICKR_KEY']}&accuracy=8&per_page=10&has_geo=1&lat=#{@lat}&lon=#{@long}&radius=20&is_getty=1&extras=geo%2Cowner_name%2C+url_s%2C+url_sq%2C&views%2C&format=json&nojsoncallback=1")
   end
 
   private
