aa9e1fafde625939b6706d09431aba17555ac9bd
Nikhil Shahi <shniks@gmail.com>
2018-06-05 17:36:06 -0600

    Added additional assertions to the park index feature test.

--- spec/features/user_can_see_location_and_parks_within_a_radius_spec.rb
+++ spec/features/user_can_see_location_and_parks_within_a_radius_spec.rb
@@ -4,10 +4,11 @@ require './spec/fixtures/omniauth/omniauth_mock'
 describe 'As a User' do
   describe 'I log into the app' do
     describe 'And I enter my street, city, state address into the search fields' do
-      describe 'And I enter a radius in miles' do
+      describe 'And I enter a radius in miles and click on submit' do
         describe 'I get redirected to the parks index' do
           scenario 'I can see my location and a list of parks for my search params' do
             VCR.use_cassette('search_parks') do
+              create(:park)
               stub_omniauth
               visit root_path
 
@@ -21,18 +22,18 @@ describe 'As a User' do
               fill_in :radius, with: 500
 
               click_on 'Submit'
+              click_on 'List View'
 
               expect(current_path).to eq(parks_path)
-              # expect(page).to have_content('73 National Parks found within 500 miles of your current location')
+              expect(page).to have_content('1 National Parks found within 500 miles of your current location')
               expect(page).to have_content('Select a park from the map or list to continue...')
-              # Need to stub out geocoder
-              # expect(page).to have_content('73 parks found within your selected range.')
-              # expect(page).to have_css('.park', count: 73)
-              # within(first('.park')) do
-              #   expect(page).to have_css('.name')
-              #   expect(page).to have_css('.state')
-              #   expect(page).to have_css('.description')
-              # end
+              expect(page).to have_css('.park', count: 1)
+              within(first('.park')) do
+                expect(page).to have_css('.park-name')
+                expect(page).to have_content('Rocky Mountain National Park')
+                expect(page).to have_css('.park-description')
+                expect(page).to have_content('Rocky Mountain National Park is the best park in Colorado')
+              end
             end
           end
         end
