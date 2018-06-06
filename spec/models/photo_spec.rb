28a2c5834344dc0e71d2bc4298068a35b1721326
Nikhil Shahi <shniks@gmail.com>
2018-06-06 11:02:11 -0600

    Updated presenter and service tests for the updated service and model.

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -4,21 +4,17 @@ RSpec.describe Photo, type: :model do
   describe 'Initialize' do
     it 'Creates a photo object' do
       info = { title: 'Awesome Image',
-               ownername: 'Honeybooboo',
                latitude: '34.3456',
                longitude: '-115.4567',
                url_s: 'http://awesomerockymountains.com',
-               url_sq: 'http://awesomerockymountains.com/sq'
               }
 
       photo = Photo.new(info)
 
       expect(photo.title).to eq('Awesome Image')
-      expect(photo.owner).to eq('Honeybooboo')
       expect(photo.latitude).to eq('34.3456')
       expect(photo.longitude).to eq('-115.4567')
       expect(photo.url).to eq('http://awesomerockymountains.com')
-      expect(photo.url_sq).to eq('http://awesomerockymountains.com/sq')
     end
   end
 end
