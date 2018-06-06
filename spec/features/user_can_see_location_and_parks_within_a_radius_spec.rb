446a8333c689057df37210b6d7388d83e167bb1f
Nikhil Shahi <shniks@gmail.com>
2018-06-05 20:59:58 -0600

    Amended park factory to create a sequence of park factories with different factory names.

--- spec/features/user_can_see_location_and_parks_within_a_radius_spec.rb
+++ spec/features/user_can_see_location_and_parks_within_a_radius_spec.rb
@@ -8,7 +8,7 @@ describe 'As a User' do
         describe 'I get redirected to the parks index' do
           scenario 'I can see my location and a list of parks for my search params' do
             VCR.use_cassette('search_parks') do
-              create(:park)
+              park = create_list(:park, 10)
               stub_omniauth
               visit root_path
 
@@ -23,14 +23,14 @@ describe 'As a User' do
 
               click_on 'Submit'
               click_on 'List View'
-
+              
               expect(current_path).to eq(parks_path)
-              expect(page).to have_content('1 National Parks found within 500 miles of your current location')
+              expect(page).to have_content('10 National Parks found within 500 miles of your current location')
               expect(page).to have_content('Select a park from the map or list to continue...')
-              expect(page).to have_css('.park', count: 1)
-              within(first('.park')) do
+              expect(page).to have_css('.park-row', count: 10)
+              within(first('.park-row')) do
                 expect(page).to have_css('.park-name')
-                expect(page).to have_content('Rocky Mountain National Park')
+                expect(page).to have_content('Rocky Mountain National Park 2')
                 expect(page).to have_css('.park-description')
                 expect(page).to have_content('Rocky Mountain National Park is the best park in Colorado')
               end
