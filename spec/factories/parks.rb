c9ce9252fb51039f5994d51e446419487210a709
Nikhil Shahi <shniks@gmail.com>
2022-08-13 13:44:53 -0700

    Updated factories to align with new format.

--- spec/factories/parks.rb
+++ spec/factories/parks.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :park do
-    state "Colorado"
-    description "Rocky Mountain National Park is the best park in Colorado"
-    directions_info "Go to Boulder and make a left to Estes Park"
-    directions_url "http://awesomerockymountains.com/directions"
-    full_name "Rocky Mountain National Park"
-    url "http://awesomerockymountains.com"
-    weather_info "Partly cloudy with a chance of shower"
-    latitude "40.3428"
-    longitude "-105.6836"
+    state { "Colorado" }
+    description { "Rocky Mountain National Park is the best park in Colorado" }
+    directions_info { "Go to Boulder and make a left to Estes Park" }
+    directions_url { "http://awesomerockymountains.com/directions" }
+    full_name { "Rocky Mountain National Park" }
+    url { "http://awesomerockymountains.com" }
+    weather_info { "Partly cloudy with a chance of shower" }
+    latitude { "40.3428" }
+    longitude { "-105.6836" }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    uid "1"
-    name "Honeybooboo"
-    first_name "Honey"
-    email "honeybooboo@hbo.com"
-    oauth_token "#2232344556!!@3$"
+    uid { "1" }
+    name { "Honeybooboo" }
+    first_name { "Honey" }
+    email { "honeybooboo@hbo.com" }
+    oauth_token { "#2232344556!!@3$" }
   end
 end
