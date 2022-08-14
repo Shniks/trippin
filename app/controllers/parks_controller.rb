2ca1e99a9f36af3229d8d0b8eae859274cbd43a3
Nikhil Shahi <shniks@gmail.com>
2022-08-13 20:22:36 -0700

    Fixed error with the code. If statement was seeking lat twice, instead of lat and long.

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,7 +4,7 @@ class ParksController < ApplicationController
   def index
     park_presenter = ParkPresenter.new(params)
     @current_location ||= park_presenter.current_location
-    if @current_location[:lat].nil? || @current_location[:lat].nil?
+    if @current_location[:lat].nil? || @current_location[:long].nil?
       flash[:failure] = 'You have entered an invalid address. Please re-enter a valid address.'
       redirect_to search_path
     else
